8cfd5ab0168a99419f23a3430529947b6c69c2ed
Marc Alburo <alburo.marc@gmail.com>
2020-12-18 15:53:54 +0800

    Created repo

---
+++ bot.py
@@ -0,0 +1,73 @@
+import os
+import discord
+import random
+import asyncio
+from discord import Game
+from discord.ext.commands import Bot
+
+import keepAlive
+
+BOT_PREFIX = ("eb ")
+TOKEN = os.getenv("TOKEN")
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.command(name='hey',
+                description="Greets the bot.",
+                brief="Hey.",
+                pass_context=True)
+async def hi(context):
+    await client.say("Hello, " + context.message.author.mention)
+
+
+@client.command(name='referee',
+                description="Initializes a referee and starts a timer for the duel.\n"
+                            + "Usage: eb referee [@player]",
+                brief="Summons the referee.",
+                aliases=['ref', 'rf'],
+                pass_context=True)
+async def referee(context):
+    refereeReplies = [
+        'I\'m still on development so... Not yet, no.',
+        'I\'m sorry, I know you need me but I have responsibilities too.',
+    ]
+    await client.say(context.message.author.mention + ", " + random.choice(refereeReplies))
+
+
+@client.command(name='8ball',
+                description="Sends your question out to space in hopes of receiving an answer.\n"
+                            + "Usage: eb 8ball [question]",
+                brief="Answers from the universe.",
+                aliases=['8b', 'eightball', '8-ball'],
+                pass_context=True)
+async def eightBall(context):
+    eightBallReplies = [
+        'That is a resounding no',
+        'It is not looking likely',
+        'Too hard to tell',
+        'It is quite possible',
+        'Definitely',
+    ]
+    await client.say(random.choice(eightBallReplies) + ", " + context.message.author.mention)
+
+
+@client.command()
+async def square(number):
+    squared_value = int(number) * int(number)
+    await client.say(str(number) + " squared is " + str(squared_value))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+keepAlive.keepAlive()
+
+client.run(TOKEN)
